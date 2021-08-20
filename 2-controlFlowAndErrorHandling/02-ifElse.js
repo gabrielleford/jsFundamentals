46468f27f3bf9c00e342699161d909d8abca2948
gabrielleford <gabrielledeva@gmail.com>
2021-08-20 16:59:30 -0400

    Finished 02-ifElse video

--- 2-controlFlowAndErrorHandling/02-ifElse.js
+++ 2-controlFlowAndErrorHandling/02-ifElse.js
@@ -1,3 +1,17 @@
+let weather = 75;
+if(weather < 70){
+    console.log('wear a jacket');
+} else {
+    console.log('No jacket necessary');
+}
+
+// you can blend complex conditionals and if-else statements based upon your need:
+if(weather > 70 && typeof weather === 'number') {
+    console.log('this weather is amazing')
+} else {
+    console.log('Either the temperature is cool, the variable is not a string, or both')
+}
+
 let age = 17;
 if(age >= 18) {
     console.log('You can vote!');

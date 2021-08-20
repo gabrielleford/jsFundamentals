a455a9668764e6dc52215d90dd0682295f5796fc
gabrielleford <gabrielledeva@gmail.com>
2021-08-20 14:35:30 -0400

    Finished 01-assignmentOperator

---
+++ 5-expressionsAndOperators/01-assignmentOperator.js
@@ -0,0 +1,16 @@
+let x = 50;
+let y = 5;
+console.log(x += y);
+console.log(x -= y);
+console.log(x *= y);
+console.log(x /= y);
+console.log(x %= y);
+console.log(x **= y);
+
+let string = 'Hello';
+console.log(string = string + y);
+console.log(string = string - y);
+console.log(string = string * y);
+console.log(string = string / y);
+console.log(string = string % y);
+console.log(string = string ** y);

3fbe77b6ba0564862290a70e98037f44ac5150e2
gabrielleford <gabrielledeva@gmail.com>
2021-08-20 16:24:35 -0400

    Went through 01-assignmentOperators video

--- 5-expressionsAndOperators/01-assignmentOperators.js
+++ 5-expressionsAndOperators/01-assignmentOperators.js
@@ -1,11 +1,52 @@
-let x = 50;
-let y = 5;
-console.log(x += y);
-console.log(x -= y);
-console.log(x *= y);
-console.log(x /= y);
-console.log(x %= y);
-console.log(x **= y);
+/*
+ASSIGNMENT OPERATORS
+
+5-expressionsAndOperators
+    01-assignmentOperators.js
+*/
+// Assignment operator
+let x = 20;
+
+// addition assignment operator
+x += 1; // x = x + 1
+console.log(x);
+
+// subtraction assignment operator
+x -= 1; // x = x - 1
+console.log(x);
+
+// multiplication assignment operator
+x *= 2; // x = x * 2
+console.log(x);
+
+// division assignment operator
+x /= 2; // x = x / 4
+console.log(x);
+
+// remainder (modulus) assignment operator
+x %= 2; // x = x % 2
+console.log(x);
+
+// modulus operator
+console.log(5 % 3); // 5/3 -> 1 2/3 -> 2
+console.log(25 % 7); // 25/7 -> 3 4/7 -> 4
+
+// exponential assignment operator
+x **= 2;
+console.log(x);
+
+let y = 7;
+y **=3;
+console.log(y);
+
+// let x = 50;
+// let y = 5;
+// console.log(x += y);
+// console.log(x -= y);
+// console.log(x *= y);
+// console.log(x /= y);
+// console.log(x %= y);
+// console.log(x **= y);
 
 let string = 'Hello';
 console.log(string = string + y);

e46e93ff99633ebd1c89d8a7c25b76026a3b005e
gabrielleford <gabrielledeva@gmail.com>
2021-08-20 14:52:56 -0400

    Setting up template for output

---
+++ 5-expressionsAndOperators/02-comparisonOperators.js
@@ -0,0 +1,63 @@
+let num1 = 5;
+let num2 = '5';
+let num3 = 6;
+let num4 = 4;
+
+console.log('Equal Operator:');
+console.log(num1 == num2);
+console.log(num1 == num3);
+console.log(num1 == num4);
+console.log('--------------------------------------');
+
+console.log('Not Equal Operator:');
+console.log(num1 != num2);
+console.log(num1 != num2);
+console.log(num1 != num2);
+console.log('--------------------------------------');
+
+console.log('Strict Equal Operator:');
+console.log(num1 != num2);
+console.log(num1 != num2);
+console.log(num1 != num2);
+console.log('--------------------------------------');
+
+console.log('Strict Not Equal Operator:');
+console.log(num1 != num2);
+console.log(num1 != num2);
+console.log(num1 != num2);
+console.log('--------------------------------------');
+
+console.log('Greater Than Operator:');
+console.log(num1 != num2);
+console.log(num1 != num2);
+console.log(num1 != num2);
+console.log('--------------------------------------');
+
+console.log('Greater Than or Equal Operator:');
+console.log(num1 != num2);
+console.log(num1 != num2);
+console.log(num1 != num2);
+console.log('--------------------------------------');
+
+console.log('Less Than Operator:');
+console.log(num1 != num2);
+console.log(num1 != num2);
+console.log(num1 != num2);
+console.log('--------------------------------------');
+
+console.log('Less Than or Equal Operator:');
+console.log(num1 != num2);
+console.log(num1 != num2);
+console.log(num1 != num2);
+console.log('--------------------------------------');
+
+console.log('AND Operator:');
+console.log(num1 != num2);
+console.log(num1 != num2);
+console.log(num1 != num2);
+console.log('--------------------------------------');
+
+console.log('OR Operator:');
+console.log(num1 != num2);
+console.log(num1 != num2);
+console.log(num1 != num2);

04d289007f58117d29a4c165bfdbf0ceb8d6561a
gabrielleford <gabrielledeva@gmail.com>
2021-08-20 14:42:19 -0400

    Finished 01-comments

---
+++ 01-comments.js
@@ -0,0 +1,17 @@
+// This line is commented out
+console.log('This is underneath the comment and will run.')
+
+/*
+multiple lines commented
+out in a block
+Remember that commented code will not run.
+None of this code will run.
+*/
+console.log('Howdy');
+
+let myAge = 28;
+/*
+Multi-line comment
+Comment here
+*/
+console.log(myAge);

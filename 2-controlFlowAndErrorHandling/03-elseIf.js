dfc52bb28ec2ddc31db515ed73eabfdab0c8a226
gabrielleford <gabrielledeva@gmail.com>
2021-08-20 16:07:53 -0400

    Finished 03-elseIf

--- 2-controlFlowAndErrorHandling/03-elseIf.js
+++ 2-controlFlowAndErrorHandling/03-elseIf.js
@@ -1,4 +1,10 @@
-let roomClean = true;
-if(roomClean) {
-    console.log('I can take the Ferrari out!');
+let age = 17;
+if(age >= 25) {
+    console.log('You can rent a car.');
+} else if(age < 25 && age >= 21) {
+    console.log('You can have a beer.');
+} else if(age < 21 && age >= 18) {
+    console.log('You can vote');
+} else {
+    console.log('You\'re just a kid! Go play with your dollies!');
 }
